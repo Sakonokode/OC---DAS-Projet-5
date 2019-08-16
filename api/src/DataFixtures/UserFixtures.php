6cc851f29d9a3ead7e13065e4285df34a0648ef8
Sakonokode <maxence.attal@gmail.com>
2019-08-17 00:10:40 +0200

    Create UserFixtures, update docker's php version to get rid of a bug with doctrine-fixtures in php 7.2

---
+++ api/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class UserFixtures
+ * @package App\DataFixtures
+ */
+final class UserFixtures extends Fixture
+{
+    /** @var UserPasswordEncoderInterface $passEncoder */
+    private $passEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $users = Yaml::parseFile(__DIR__ . '/fixtures/users.yaml');
+
+        foreach ($users['users'] as $nickName => $user) {
+
+            $user = $this->instantiate(
+                $user['email'],
+                $user['password'],
+                $user['roles']
+            );
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @param string|null $email
+     * @param string|null $password
+     * @param array|null $roles
+     * @return User
+     */
+    public function instantiate(
+        string $email = null,
+        string $password = null,
+        array $roles = null
+    ): User
+    {
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword($this->passEncoder->encodePassword($user, $password));
+        $user->setRoles($roles);
+
+        return $user;
+    }
+}
